043bdf0324acece3f92d053648164840f0353992
Federico Calvette <calvettefederico@gmail.com>
2023-01-05 13:19:10 -0300

    Add docstrings

--- app/app.py
+++ app/app.py
@@ -8,25 +8,40 @@ app.config['SECRET_KEY'] = settings.SECRET_KEY
 
 @app.route('/', methods=['GET'])
 def home():
+    """Esta funcion retorna el contenido para el home de la web de demo, solo acepta peticiones GET. 
+
+    Returns:
+        render_template: Es un str donde se encuentra el contenído HTML de la pagina web. 
+    """    
     return render_template('index.html')
 
 @app.route('/validation/', methods=['POST'])
 def validation():
+    """Funcion que se encarga de llamar al validador de cuentas (API de Prometeo). Controla la respuesta del mismo y retorna un mensaje para el front.
 
+    Returns:
+        str: Retorna un string donde se denota el estado de la cuenta o de la solicitud en si. Informando errores o casos satisfactorios. 
+    """    
+
+    #Variables que recibo desde el form-front y utilizo para consultar a Prometeo por la cuenta.
     numero_cuenta = request.form['numero_cuenta']
     codigo_banco = request.form['codigo_banco']
     codigo_pais = request.form['codigo_pais']
 
+    #Control de parametros previo a la consulta.
+    #Controlo que no falten parametros requeridos, ya que afectaría la consulta.  
     if not numero_cuenta:
         return 'Error en el formulario, el número de cuenta es requerido!'
     
-    elif not codigo_banco and codigo_pais == 'UY':
+    elif not codigo_banco and codigo_pais != 'PE':
         return 'Error en el formulario, el código de banco es requerido!'
     
     elif not codigo_pais:
         return 'Error en el formulario, el código de país es requerido!'
     
     else:
+        #Si pasa el control de parametros llamo a make_request de prometeo_request, enviando los parametros de numero_cuenta, codigo_banco, codigo_pais.
+        #Una vez obtengo la respuesta la guardo en response_request y procedo a enviar el mensaje al front, en funcion de la respuesta obtenida. 
         response_request = prometeo_request.make_request(numero_cuenta, codigo_banco, codigo_pais)
 
         if response_request == "Cuenta credito invalida": return response_request
@@ -34,6 +49,10 @@ def validation():
         elif response_request == "Error de comunicacion con el banco": return response_request
 
         elif "Parametros invalidos" in response_request: return response_request
+
+        elif "Error de tipo exception:" in response_request: return response_request
+
+        elif "Error inesperado" in response_request: return response_request
 
         else: return f'El titular de la cuenta bancaria {numero_cuenta} se llama: {response_request}.'
              
